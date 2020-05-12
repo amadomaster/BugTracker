8a654cdb2bb8516822499fb4ca0ce280d6307017
Amado <amadomaster@gmail.com>
2020-05-12 17:17:00 -0500

    Step 4: Generate and import JDL

--- src/main/java/mx/uady/bugtracker/config/CacheConfiguration.java
+++ src/main/java/mx/uady/bugtracker/config/CacheConfiguration.java
@@ -45,6 +45,11 @@ public class CacheConfiguration {
             createCache(cm, mx.uady.bugtracker.domain.User.class.getName());
             createCache(cm, mx.uady.bugtracker.domain.Authority.class.getName());
             createCache(cm, mx.uady.bugtracker.domain.User.class.getName() + ".authorities");
+            createCache(cm, mx.uady.bugtracker.domain.Project.class.getName());
+            createCache(cm, mx.uady.bugtracker.domain.Label.class.getName());
+            createCache(cm, mx.uady.bugtracker.domain.Label.class.getName() + ".tickets");
+            createCache(cm, mx.uady.bugtracker.domain.Ticket.class.getName());
+            createCache(cm, mx.uady.bugtracker.domain.Ticket.class.getName() + ".labels");
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/main/java/mx/uady/bugtracker/repository/LabelRepository.java
@@ -0,0 +1,14 @@
+package mx.uady.bugtracker.repository;
+
+import mx.uady.bugtracker.domain.Label;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Label entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface LabelRepository extends JpaRepository<Label, Long> {
+}
